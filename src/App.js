494e4888221da0a7dbe36aa7a8d87f83a6502b83
DaehunKim <gidonskaris@gmail.com>
2020-09-08 20:31:56 +0900

    [Add] CSS in Header (styled-component)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Router from "./Components/Router";
 import './App.css';
+import GlobalStyles from "./Components/GlobalStyles";
 
 
 function App() {
   return (
     <>
         <Router/>
+        <GlobalStyles/>
     </>
   );
 }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,21 +1,58 @@
 import React from 'react';
 import {Link} from "react-router-dom";
+import styled from "styled-components";
+
+
+const Head = styled.header`
+  color: white;
+  position: fixed;
+  top : 0;
+  left : 0;
+  width: 100%;
+  height: 50px;
+  display: flex;
+  align-items: center;
+  background-color: rgba(20,20,20,0.8);
+  z-index : 10;
+  box-shadow: 0px 1px 5px 2px rgba(0,0,0,0.8);
+`;
+
+const List = styled.ul`
+  display: flex;
+`;
+
+const Item = styled.li`
+  width: 80px;
+  height: 50px;
+  text-align: center;
+  font-size: 20px;
+  font-weight: 600;
+`;
+
+const SLINK = styled(Link)`
+  height: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`; //함수를 스타일링 할때 (LINK의 경우 react-router-dom에서 불러온 함수이기 때문에)
+
+
 
 const Header = () => {
     return (
-        <header>
-            <ul>
-                <li>
-                    <Link to={"/"}>Movies</Link>
-                </li>
-                <li>
-                    <Link to={"/TV"}>TV</Link>
-                </li>
-                <li>
-                    <Link to={"/Search"}>Search</Link>
-                </li>
-            </ul>
-        </header>
+        <Head>
+            <List>
+                <Item>
+                    <SLINK to={"/"}>Movies</SLINK>
+                </Item>
+                <Item>
+                    <SLINK to={"/TV"}>TV</SLINK>
+                </Item>
+                <Item>
+                    <SLINK to={"/Search"}>Search</SLINK>
+                </Item>
+            </List>
+        </Head>
     );
 };
 
