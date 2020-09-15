347847eafca94ccfe0f6fd7b16b47a872c055847
DaehunKim <gidonskaris@gmail.com>
2020-09-15 20:48:42 +0900

    [Add] movies presenter #1

--- src/Routes/Movie/MoviePresenter.js
+++ src/Routes/Movie/MoviePresenter.js
@@ -1,11 +1,45 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from "styled-components";
+
+
+
+const Container = styled.div`
+  padding : 0px 15px; 
+`;
 
 const MoviePresenter = ({nowPlaying, popular, topRated,upComing, error, loading}) => {
     return (
-        <div>
-            <h1>this is {nowPlaying.length}</h1>
-        </div>
+        <Container>
+            {nowPlaying && nowPlaying.length > 0 && (
+                <div>
+                    {nowPlaying.map(movie => (
+                        <h1>{movie.title}</h1>
+                    ))}
+                </div>
+            )}
+            {popular && popular.length > 0 && (
+                <div>
+                    {popular.map(movie => (
+                        <h1>{movie.popularity}</h1>
+                    ))}
+                </div>
+            )}
+            {topRated && topRated.length > 0 && (
+                <div>
+                    {topRated.map(movie => (
+                        <h1>{movie.vote_count}</h1>
+                    ))}
+                </div>
+            )}
+            {upComing && upComing.length > 0 && (
+                <div>
+                    {upComing.map(movie => (
+                        <h1>{movie.video}</h1>
+                    ))}
+                </div>
+            )}
+        </Container>
     );
 };
 
