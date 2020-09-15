650ca5c5db0f5543ca04db479bd1071f0be5807b
DaehunKim <gidonskaris@gmail.com>
2020-09-15 21:08:20 +0900

    [Add] loader

---
+++ src/Components/Loader.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  font-size: 48px;
+  margin-top: 80px;
+`;
+
+const Loader = () => {
+    return (
+        <Container>
+            <span role={"img"} aria-label={"loading"}>
+                😀
+            </span>
+        </Container>
+    );
+};
+
+export default Loader;
--- src/Routes/Movie/MoviePresenter.js
+++ src/Routes/Movie/MoviePresenter.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from "styled-components";
 import Section from "../../Components/Section";
+import Loader from "../../Components/Loader";
 
 
 
@@ -9,38 +10,47 @@ const Container = styled.div`
   padding : 0px 15px; 
 `;
 
-const MoviePresenter = ({nowPlaying, popular, topRated,upComing, error, loading}) => {
+const MoviePresenter = ({
+    nowPlaying,
+    popular,
+    topRated,
+    upComing,
+    error,
+    loading
+}) => {
     return (
-        <Container>
-            {nowPlaying && nowPlaying.length > 0 && (
-                <Section title={"NowPlaying"}>
-                    {nowPlaying.map(movie => (
-                        <h1>{movie.title}</h1>
-                    ))}
-                </Section>
-            )}
-            {popular && popular.length > 0 && (
-                <Section title={"Popular"}>
-                    {popular.map(movie => (
-                        <h1>{movie.popularity}</h1>
-                    ))}
-                </Section>
-            )}
-            {topRated && topRated.length > 0 && (
-                <Section title={"Top Rated"}>
-                    {topRated.map(movie => (
-                        <h1>{movie.vote_count}</h1>
-                    ))}
-                </Section>
-            )}
-            {upComing && upComing.length > 0 && (
-                <Section title={"Upcoming"}>
-                    {upComing.map(movie => (
-                        <h1>{movie.title}</h1>
-                    ))}
-                </Section>
-            )}
-        </Container>
+        loading
+            ? <Loader/>
+            :  <Container>
+                {nowPlaying && nowPlaying.length > 0 && (
+                    <Section title={"NowPlaying"}>
+                        {nowPlaying.map(movie => (
+                            <h1>{movie.title}</h1>
+                        ))}
+                    </Section>
+                )}
+                {popular && popular.length > 0 && (
+                    <Section title={"Popular"}>
+                        {popular.map(movie => (
+                            <h1>{movie.popularity}</h1>
+                        ))}
+                    </Section>
+                )}
+                {topRated && topRated.length > 0 && (
+                    <Section title={"Top Rated"}>
+                        {topRated.map(movie => (
+                            <h1>{movie.vote_count}</h1>
+                        ))}
+                    </Section>
+                )}
+                {upComing && upComing.length > 0 && (
+                    <Section title={"Upcoming"}>
+                        {upComing.map(movie => (
+                            <h1>{movie.title}</h1>
+                        ))}
+                    </Section>
+                )}
+            </Container>
     );
 };
 
